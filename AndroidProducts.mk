edf0a4f735a121e908aa63772569ea8c2cbf4459
aliut <ali.shahid.latif3@gmail.com>
2022-05-11 02:57:06 -0700

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_NX709J.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_NX709J-user \
+    twrp_NX709J-userdebug \
+    twrp_NX709J-eng
